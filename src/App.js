48648b45511a19fa8de77691baea9691b90ddabe
Shahoriar <shahoriar.fahim@gmail.com>
2020-05-27 15:00:37 -0700

    Fetch data and show it web page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 
@@ -16,6 +16,8 @@ const produts = [                //Object
       <header className="App-header">
 
         <p>My first react paragraph</p>
+
+      <Users></Users>
 
         <Counter></Counter>
 
@@ -38,6 +40,28 @@ const produts = [                //Object
     </div>
   );
 }
+
+function Users(){
+  const[users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data));
+  }, [])
+
+  return(
+    <div>
+      <h3>Dynamic Users: {users.length}</h3>
+      <ul>
+        {
+          users.map(user => <li>{user.name}</li>)
+        }
+      </ul>
+    </div>
+  )
+}
+
 
 function Counter(){
   const [count, setCount] = useState(0);
